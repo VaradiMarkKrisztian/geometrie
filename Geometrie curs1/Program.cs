1b4e09edccae88a0127b4de6559d437b62c8340f
VaradiMark <72255624+VaradiMarkKrisztian@users.noreply.github.com>
2021-03-10 11:29:40 +0200

    added 1-3

---
+++ Geometrie curs1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometrie_curs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("doubleroduceti coordonate");
+            Console.WriteLine("Xa=");
+            double Xa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ya=");
+            double Ya = double.Parse(Console.ReadLine()); 
+            Console.WriteLine("Za=");
+            double Za = double.Parse(Console.ReadLine());
+            Console.WriteLine("Xb=");
+            double Xb = double.Parse(Console.ReadLine());
+            Console.WriteLine("Yb=");
+            double Yb = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zb=");
+            double Zb = double.Parse(Console.ReadLine());
+            Console.WriteLine("Xc=");
+            double Xc = double.Parse(Console.ReadLine());
+            Console.WriteLine("Yc=");
+            double Yc = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zc=");
+            double Zc = double.Parse(Console.ReadLine());
+            ecDreapta(Xa,Xb,Yb,Ya);
+            ecDistanta(Xa, Xb, Yb, Ya);
+            Coliniar(Xa, Xb, Yb, Ya, Xc, Yc);
+            Concurente(Xa, Xb, Yb, Ya, Xc, Yc,Za,Zb,Zc);
+           
+            
+
+        }
+
+        private static void Concurente(double xa, double xb, double yb, double ya, double xc, double yc, double za, double zb, double zc)
+        {
+            if ( ya >= 0 && za >= 0) Console.WriteLine($"d1={xa}x+{ya}y+{za}=0");
+            else if ( ya < 0 && za >= 0) Console.WriteLine($"d1={xa}x{ya}y+{za}=0");
+            else if (ya < 0 && za < 0) Console.WriteLine($"d1={xa}x{ya}y{za}=0");
+            else if (ya >= 0 && za < 0) Console.WriteLine($"d1={xa}x+{ya}y{za}=0");
+
+            if (yb >= 0 && zb >= 0) Console.WriteLine($"d2={xb}x+{yb}y+{zb}=0");
+            else if (yb < 0 && zb >= 0) Console.WriteLine($"d2={xb}x{ya}y+{zb}=0");
+            else if (yb < 0 && zb < 0) Console.WriteLine($"d2={xb}x{yb}y{zb}=0");
+            else if (yb >= 0 && zb < 0) Console.WriteLine($"d2={xb}x+{yb}y{zb}=0");
+
+            if (yc >= 0 && zc >= 0) Console.WriteLine($"d3={xc}x+{yc}y+{zc}=0");
+            else if (yc < 0 && zc >= 0) Console.WriteLine($"d3={xc}x{yc}y+{zc}=0");
+            else if (yc < 0 && zc < 0) Console.WriteLine($"d3={xc}x{yc}y{zc}=0");
+            else if (yc >= 0 && zc < 0) Console.WriteLine($"d3={xc}x+{yc}y{zc}=0");
+
+            double a = xa * yb * zc;
+            double b = xb * yc * za;
+            double c = xc * ya * zb;
+            double d = xc * yb * za;
+            double e = zb * yc * xa;
+            double f = zc * ya * xb;
+
+            if (a + b + c - d - e - f == 0) Console.WriteLine("Sunt concurente");
+            else if (a + b + c - d - e - f != 0) Console.WriteLine("Nu sunt concurente");
+
+            Console.ReadKey();
+        }
+
+        private static void Coliniar(double xa, double xb, double yb, double ya, double xc, double yc)
+        {
+            double a = xa * yb;
+            double b = xb * yc;
+            double c = xc * xa;
+            double d = xc * yb;
+            double e = xa * yc;
+            double f = xb * ya;
+
+            if (a + b + c - d - e - f == 0) Console.WriteLine("Este coliniar");
+            else if(a + b + c - d - e - f != 0) Console.WriteLine("NU este coliniar");
+            
+        }
+
+        private static void ecDistanta(double Xa, double Xb, double Yb, double Ya)
+        {
+            double a = Math.Sqrt(Math.Pow((Xb - Xa),2) + Math.Pow((Yb - Ya),2));
+            Console.WriteLine(a);
+             
+        }
+
+        private static void ecDreapta(double Xa,double Xb,double Yb,double Ya)
+        {
+            double a = Ya - Yb;
+            double b = Xa - Xb;
+            double c = Xa * Yb - (Xb * Ya);
+             if( b < 0 && c < 0) Console.WriteLine($"{a}x {-b}y {c}=0");
+            else if ( b < 0 && c >= 0) Console.WriteLine($"{a}x +{-b}y +{c}=0");
+            else if ( b > 0 && c >= 0) { Console.WriteLine($"{a}x -{b}y +{c}=0"); }
+            else if ( b > 0 && c < 0) { Console.WriteLine($"{a}x -{b}y {c}=0"); }
+            
+        }
+    }
+}
---
+++ geometrie curs2/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometrie_curs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                string name1 = Console.ReadLine();
+                int points1 = Convert.ToInt32(Console.ReadLine());
+                string name2 = Console.ReadLine();
+                int points2 = Convert.ToInt32(Console.ReadLine());
+
+                DancerPoints dancer1 = new DancerPoints(name1, points1);
+                DancerPoints dancer2 = new DancerPoints(name2, points2);
+
+                DancerPoints total = dancer1 + dancer2;
+                Console.WriteLine(total.name);
+                Console.WriteLine(total.points);
+            Console.ReadKey();
+        }
+
+        class DancerPoints
+        {
+            public string name;
+            public int points;
+            public DancerPoints(string name, int points)
+            {
+                this.name = name;
+                this.points = points;
+            }
+
+            //overload the + operator
+            public static DancerPoints operator+(DancerPoints a, DancerPoints b)
+            {
+                string name = a.name + b.name;
+                int points = a.points + b.points;
+                DancerPoints total = new DancerPoints(name, points);
+                return total;
+            }
+        }
+        
+            //ex1();
+            //ex2();
+           // ex3();
+    
+
+        private static void ex3()
+        {
+            Console.WriteLine("introduceti coordonate pentru vector1");
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int z1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduceti coordonate pentru vector2");
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            int z2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduceti coordonate pentru vector3");
+            int x3 = int.Parse(Console.ReadLine());
+            int y3 = int.Parse(Console.ReadLine());
+            int z3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("v1= ");
+            if (y1 >= 0 && z1 > 0) Console.WriteLine($"{x1}i+{y1}j+{z1}k");
+            if (y1 < 0 && z1 > 0) Console.WriteLine($"{x1}i{y1}j+{z1}k");
+            if (y1 >= 0 && z1 < 0) Console.WriteLine($"{x1}i+{y1}j{z1}k");
+            if (y1 < 0 && z1 < 0) Console.WriteLine($"{x1}i{y1}j{z1}k");
+            if (y1 >= 0 && z1 == 0) Console.WriteLine($"{x1}i+{y1}j");
+            if (y1 < 0 && z1 == 0) Console.WriteLine($"{x1}i{y1}j");
+            Console.WriteLine("v2= ");
+            if (y2 >= 0 && z2 > 0) Console.WriteLine($"{x2}i+{y2}j+{z2}k");
+            if (y2 < 0 && z2 > 0) Console.WriteLine($"{x2}i{y2}j+{z2}k");
+            if (y2 >= 0 && z2 < 0) Console.WriteLine($"{x2}i+{y2}j{z2}k");
+            if (y2 < 0 && z2 < 0) Console.WriteLine($"{x2}i{y2}j{z2}k");
+            if (y2 >= 0 && z2 == 0) Console.WriteLine($"{x2}i+{y2}j");
+            if (y2 < 0 && z2 == 0) Console.WriteLine($"{x2}i{y2}j");
+            Console.WriteLine("v2= ");
+            if (y3 >= 0 && z3 > 0) Console.WriteLine($"{x3}i+{y3}j+{z3}k");
+            if (y3 < 0 && z3 > 0) Console.WriteLine($"{x3}i{y3}j+{z3}k");
+            if (y3 >= 0 && z3 < 0) Console.WriteLine($"{x3}i+{y3}j{z3}k");
+            if (y3 < 0 && z3 < 0) Console.WriteLine($"{x3}i{y3}j{z3}k");
+            if (y3 >= 0 && z3 == 0) Console.WriteLine($"{x3}i+{y3}j");
+            if (y3 < 0 && z3 == 0) Console.WriteLine($"{x3}i{y3}j");
+            Console.WriteLine( "Produs mixt: ");
+            double produsmixt = x1 * y2 * z3 + x2 * y3 * z1 + x3 * y1 * z2 - x3 * y2 * z1 - y3 * z2 * x1 - z3 * x2 * y1;
+            Console.WriteLine(produsmixt);
+            Console.WriteLine("Verificare Coplanaritate");
+            if (produsmixt == 0) Console.WriteLine("Este coplanar");
+            else Console.WriteLine("Nu este coplanar");
+            Console.WriteLine("Volum paralelogram");
+            Console.WriteLine(Math.Abs(produsmixt));
+            Console.ReadKey();
+        }
+
+        private static void ex2()
+        {
+            Console.WriteLine("introduceti coordonate pentru vector1");
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int z1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduceti coordonate pentru vector2");
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            int z2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("A)afisare");
+            Console.WriteLine("v1= ");
+            if (y1 >= 0 && z1 >= 0) Console.WriteLine($"{x1}i+{y1}j+{z1}k");
+            if (y1 < 0 && z1 >= 0) Console.WriteLine($"{x1}i{y1}j+{z1}k");
+            if (y1 >= 0 && z1 < 0) Console.WriteLine($"{x1}i+{y1}j{z1}k");
+            if (y1 < 0 && z1 < 0) Console.WriteLine($"{x1}i{y1}j{z1}k");
+            Console.WriteLine("v2= ");
+            if (y2 >= 0 && z2 >= 0) Console.WriteLine($"{x2}i+{y2}j+{z2}k");
+            if (y2 < 0 && z2 >= 0) Console.WriteLine($"{x2}i{y2}j+{z2}k");
+            if (y2 >= 0 && z2 < 0) Console.WriteLine($"{x2}i+{y2}j{z2}k");
+            if (y2 < 0 && z2 < 0) Console.WriteLine($"{x2}i{y2}j{z2}k");
+            Console.WriteLine("A)Produs vectorial= ");
+            double a = Math.Sqrt(x1 * x1 + y1 * y1 + z1 * z1);
+            double b = Math.Sqrt(x2 * x2 + y2 * y2 + z2 * z2);
+            double c = x1 * x2 + y1 * y2 + z1 * z2;
+            int A1 = y1 * z2 - y2 * z1;
+            int A2 = x1 * z2 - x2 * z1;
+            int A3 = x1 * y2 - x2*y1;
+            if(A2 >= 0 && A3 >= 0) Console.WriteLine($"{A1}i-{A2}j+{A3}k");
+            if (A2 < 0 && A3 >= 0) Console.WriteLine($"{A1}i{A2}j+{A3}k");
+            if (A2 >= 0 && A3 < 0) Console.WriteLine($"{A1}i-{A2}j{A3}k");
+            if (A2< 0 && A3 < 0) Console.WriteLine($"{A1}i{A2}j{A3}k");
+            
+            Console.WriteLine();
+            Console.WriteLine("B)coliniaritate");
+            if (A1 - A2 - A3 == 0) Console.WriteLine("Este coliniar");
+            else Console.WriteLine("Nu este coliniar");
+            Console.WriteLine("C)Aria paralelogram");
+            double arie = Math.Sqrt(A1*A1+A2*A2+A3*A3);
+            Console.WriteLine(arie);
+            Console.ReadKey();
+        }
+
+        private static void ex1()
+        {
+            Console.WriteLine("introduceti coordonate pentru vector1");
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int z1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduceti coordonate pentru vector2");
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            int z2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("A)afisare");
+            Console.WriteLine("v1= ");
+            if (y1 >= 0 && z1 >= 0) Console.Write($"{x1}i+{y1}j+{z1}k=");
+            if (y1 < 0 && z1 >= 0) Console.Write($"{x1}i{y1}j+{z1}k");
+            if (y1 >= 0 && z1 < 0) Console.Write($"{x1}i+{y1}j{z1}k");
+            if (y1 < 0 && z1 < 0) Console.Write($"{x1}i{y1}j{z1}k");
+            Console.WriteLine("v2= ");
+            if (y2 >= 0 && z2 >= 0) Console.Write($"{x2}i+{y2}j+{z2}k");
+            if (y2 < 0 && z2 >= 0) Console.Write($"{x2}i{y2}j+{z2}k");
+            if (y2 >= 0 && z2 < 0) Console.Write($"{x2}i+{y2}j{z2}k");
+            if (y2 < 0 && z2 < 0) Console.Write($"{x2}i{y2}j{z2}k");
+            Console.WriteLine("B)produs scalar");
+            Console.WriteLine($"v1*v2= {x1*x2+y1*y2+z1*z2}");
+            if (x1 * x2 + y1 * y2 + z1 * z2 == 0) Console.WriteLine(" este perpendicular");
+            else Console.WriteLine("Nu este perpendicular");
+            Console.WriteLine("C)marimea vectorului");
+            Console.WriteLine($"marimea vectorului v1=");
+            double v1 = Math.Sqrt(x1 * x1 + y1 * y1 + z1 * z1);
+            Console.Write(v1);
+            Console.WriteLine($"marimea vectorului v2=");
+            double v2 = Math.Sqrt(x2 * x2 + y2 * y2 + z2 * z2);
+            Console.Write(v2);
+            Console.ReadKey();
+        }
+    }
+}
+
